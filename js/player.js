1a5a718ae5eb427f83c813e100250e755050f4db
Joanna Zielinska <joannazielinskam@gmail.com>
2022-08-22 12:23:02 +0200

    Solved player movement

--- js/player.js
+++ js/player.js
@@ -3,32 +3,38 @@ class Player {
         this.pos = new p5.Vector(300,300)
         this.height = 50
         this.width = 50       
-        this.reach = 10
-        this.gravity = .5
+        this.reach = 10        
 	    this.velocity 
         this.acceleration
+        this.angle = 0
         
     }
 
     draw() {    
-        let dx =  mouseX - this.pos.x 
-         let dy =  mouseY - this.pos.y
-
-
+        angleMode(DEGREES) 
+        let dx =  mouseX - (this.pos.x + this.width/2)
+        let dy =  mouseY - (this.pos.y + this.height/2)
          
 
         this.velocity = createVector(dx, dy) 
         this.velocity.setMag(this.reach) 
     
-        this.acceleration = createVector(mouseX - this.pos.x, mouseY - this.pos.y).setMag(0.2) // pryzspieszenie
+        this.acceleration = createVector(mouseX - this.pos.x, mouseY - this.pos.y).setMag(0.2) 
         
         this.velocity.add(this.acceleration)
         this.velocity.limit(2)
 
       
         this.pos.add(this.velocity)
+
+        translate (this.pos.x, this.pos.y)
+        this.angle = this.velocity.heading()    
+        rotate(this.angle) 
+
+
+
  
-        image(game.playerImg, this.pos.x, this.pos.y, this.height , this.width)
+        image(game.playerImg, this.pos.x - this.width/2 , this.pos.y - this.height/2, this.width , this.height)
        
         
        
